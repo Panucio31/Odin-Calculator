bc91636253a6e1ce98398febdf3673f3c504ffcd
Panucio31 <m.januska1990@gmail.com>
2022-09-14 21:07:50 +0300

    feat: :construction: math functions done

--- script.js
+++ script.js
@@ -1,1 +1,38 @@
-// Time to script!
+const clear = document.getElementsByClassName("clear-all");
+const display = document.querySelector(".display-field");
+const num1 = document.querySelector(".one").dataset.one;
+const num2 = document.querySelector(".two").dataset.two;
+const num3 = document.querySelector(".three").dataset.three;
+const num4 = document.querySelector(".four").dataset.four;
+const num5 = document.querySelector(".five").dataset.five;
+const num6 = document.querySelector(".six").dataset.six;
+const num7 = document.querySelector(".sev").dataset.sev;
+const num8 = document.querySelector(".eight").dataset.eight;
+const num9 = document.querySelector(".nine").dataset.nine;
+const num = document.querySelector(".zero").dataset.zero;
+const numbers = document.querySelector("#num");
+
+const add = (a, b) => (a + b).toFixed(7);
+const minus = (a, b) => (a - b).toFixed(7);
+const divide = (a, b) => (a / b).toFixed(7);
+const multiply = (a, b) => (a * b).toFixed(7);
+
+const addBtn = document.querySelector(".add");
+addBtn.addEventListener("click", () => {
+  add();
+});
+
+const substractBtn = document.querySelector('.minus');
+substractBtn.addEventListener('click', () => {
+  minus();
+})
+
+const divideBtn = document.querySelector('.divide');
+divideBtn.addEventListener('click', () => {
+  divide();
+})
+
+const multiplyBtn = document.querySelector('.multiply');
+multiplyBtn.addEventListener('click', () => {
+  multiply();
+})
